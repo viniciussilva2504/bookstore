530325976db735dc1032d2da72a4513c2dda90cf
Vinic <vinic@gamestore.com>
2026-01-30 10:24:16 +0000

    implementa django-debug-toolbar e rev paginação

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -142,8 +142,3 @@ REST_FRAMEWORK = {
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 5,
-}
