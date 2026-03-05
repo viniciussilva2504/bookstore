2d24e3cff9af7f68d9c983fcd0ceadddc0f8dea4
Vinic <vinic@gamestore.com>
2026-03-05 20:53:52 +0000

    setup rev 2

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,10 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-
-from rest_framework.authentication import (
-    SessionAuthentication,
-    BasicAuthentication,
-    TokenAuthentication,
-)
 from rest_framework.permissions import IsAuthenticated
 
 from product.models import Product
@@ -12,11 +6,6 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [
-        SessionAuthentication,
-        BasicAuthentication,
-        TokenAuthentication,
-    ]
     permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
