4447a8f9d190db576be03755351567003a738fb4
Vinic <vinic@gamestore.com>
2026-03-17 21:24:31 +0000

    inseriu README.md

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,12 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
 
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
